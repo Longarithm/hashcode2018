d91b82d160be54efbf7244289868a397119507b5
sinesight <sinesight@gmail.com>
2018-03-01 18:06:14 +0300

    hello world

---
+++ test.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <ctime>
+#include <cmath>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <cassert>
+#include <queue>
+#include <bitset>
+
+using namespace std;
+
+#define clr(a) memset(a, 0, sizeof(a))
+#define pb push_back
+#define forab(i, a, b) for(int i = int(a); i < int(b); ++i)
+#define forba(i, b, a) for(int i = int(b) - 1; i >= int(a); --i)
+#define forn(i, n) forab(i, 0, n)
+
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+    goto ko;
+    cout << "ne ko\n";
+ko:
+    cout << "ko\n";
+
+    return 0;
+}
+
